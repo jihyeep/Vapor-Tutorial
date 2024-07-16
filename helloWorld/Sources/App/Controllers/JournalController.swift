277fc60ca35b7f449daa466f782d65a1e88116c9
Jihye Park <deviloper.park@gmail.com>
2024-07-16 11:03:36 +0900

    RouteCollection 프로토콜을 이용한 JournalController를 AppRouter에 직접 등록

--- helloWorld/Sources/App/Controllers/JournalController.swift
+++ helloWorld/Sources/App/Controllers/JournalController.swift
@@ -7,19 +7,32 @@
 
 import Vapor
 
-// Entry 모델에 대한 CRUD
-struct JournalController {
+// Entry 모델에 대한 CRUD (Create, Read (Get 1, or List), Update, Delete)
+struct JournalController: RouteCollection {
 
+    func boot(routes: any Vapor.RoutesBuilder) throws {
+        let entries = routes.grouped("entries")
+        entries.get(use: index)
+        entries.post(use: create)
+        entries.get(":id", use: get)
+        entries.put(":id", use: update)
+        entries.delete(":id", use: delete)
+    }
+    
+    // List
     @Sendable
     func index(req: Request) throws -> EventLoopFuture<[Entry]> {
         return Entry.query(on: req.db).all()
     }
 
+    @Sendable
     func create(req: Request) throws -> EventLoopFuture<Entry> {
         let entry = try req.content.decode(Entry.self)
         return entry.save(on: req.db).map { entry }
     }
 
+    // Read
+    @Sendable
     func get(req: Request) throws -> EventLoopFuture<Entry> {
         guard let id = req.parameters.get("id", as: UUID.self) else {
             throw Abort(.badRequest)
@@ -28,6 +41,7 @@ struct JournalController {
             .unwrap(or: Abort(.notFound))
     }
 
+    @Sendable
     func update(req: Request) throws -> EventLoopFuture<Entry> {
         guard let id = req.parameters.get("id", as: UUID.self) else {
             throw Abort(.badRequest)
@@ -42,6 +56,7 @@ struct JournalController {
             }
     }
 
+    @Sendable
     func delete(req: Request) throws -> EventLoopFuture<HTTPStatus> {
         guard let id = req.parameters.get("id", as: UUID.self) else {
             throw Abort(.badRequest)
--- helloWorld/Sources/App/routes.swift
+++ helloWorld/Sources/App/routes.swift
@@ -3,7 +3,8 @@ import Vapor
 func routes(_ app: Application) throws {
     let journalController = JournalController()
 
-    app.get("entries", use: journalController.index)
+//    app.get("entries", use: journalController.index)
+    app.get("old/entries", use: journalController.index)
     
     app.get { req async in
         "It works!"
