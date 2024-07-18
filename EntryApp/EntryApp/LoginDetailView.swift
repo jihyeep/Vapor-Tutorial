d92c11927f9346d7dab2ee89be0d32adca6b7e8a
Jihye Park <deviloper.park@gmail.com>
2024-07-18 11:15:25 +0900

    NavigationBarBackButton 커스텀

--- EntryApp/EntryApp/LoginDetailView.swift
+++ EntryApp/EntryApp/LoginDetailView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct LoginDetailView: View {
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // MARK: - 배경색
+            Color(hex: "#121A21")
+                .ignoresSafeArea(.all)
+        }
+        // MARK: - Back button
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "arrow.left")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .foregroundStyle(Color(hex: "#FFFFFF"))
+                }
+            }
+        }
     }
 }
 
