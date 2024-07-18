94426b0f9e130abee44792edaff0742df3273f54
Jihye Park <deviloper.park@gmail.com>
2024-07-18 14:35:11 +0900

    feat: AddEntryView UI 작업

--- EntryApp/EntryApp/AddEntryView.swift
+++ EntryApp/EntryApp/AddEntryView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct AddEntryView: View {
     @Environment(\.dismiss) var dismiss
+    @State private var entryTitle: String = ""
+    @State private var entryBody: String = ""
     
     var body: some View {
         NavigationStack {
@@ -18,7 +20,43 @@ struct AddEntryView: View {
                     .ignoresSafeArea(.all)
                 
                 VStack {
+                    // entryTitle
+                    TextField(text: $entryTitle) {
+                        Text("Title")
+                            .font(.system(size: 16))
+                            .foregroundStyle(Color(hex: "#94ADC7"))
+                    }
+                    // 내부 padding
+                    .padding(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16))
+                    .frame(width: 358, height: 56)
+                    .background(RoundedRectangle(cornerRadius: 8)
+                        .fill(Color(hex: "#243647")))
+                    // 외부 padding
+                    .padding(EdgeInsets(top: 12, leading: 0, bottom: 12, trailing: 0))
                     
+                    // entryBody
+                    TextEditor(text: $entryBody)
+                        .font(.system(size: 16))
+                        .foregroundStyle(Color(hex: "#94ADC7"))
+                        .padding([.leading, .trailing], 16)
+                        .frame(width: 358, height: 144)
+                        .scrollContentBackground(.hidden)
+                        .background(RoundedRectangle(cornerRadius: 8)
+                            .fill(Color(hex: "#243647")))
+                        .padding(.bottom, 12)
+                    
+                    Button() {
+                        
+                    } label: {
+                        Text("Submit")
+                            .font(.system(size: 16, weight: .bold))
+                            .foregroundStyle(Color(hex: "#FFFFFF"))
+                            .frame(width: 318, height: 48)
+                            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                            .background(RoundedRectangle(cornerRadius: 8)
+                                .fill(Color(hex: "#1A80E5")))
+                    }
+                    Spacer()
                 }
             }
             // MARK: - Navigation title/backButton
@@ -47,5 +85,5 @@ struct AddEntryView: View {
 }
 
 #Preview {
-        AddEntryView()
+    AddEntryView()
 }
